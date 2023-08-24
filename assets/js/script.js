f788a0873272cc47f6dd775db4f1763b988083f9
cshimvin <>
2023-08-24 13:56:16 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,28 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You pressed submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame("addition");
 });
 
+/**
+ * The main run function to start the game
+ */
+function runGame(gameType) {
 
-function runGame() {
+    // Creates the numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -35,8 +48,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
